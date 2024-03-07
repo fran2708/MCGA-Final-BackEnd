fe9257a268751a2b6e0c7554974dd6382801a31c
fran2708 <francopaladini2708@gmail.com>
2024-03-06 23:44:19 -0300

    feat: added connection to mongodb

--- src/index.js
+++ src/index.js
@@ -9,4 +9,9 @@ app.get('/', (req, res) => {
     res.send('test')
 })
 
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log('MongoDB connection was succesful'))
+    .catch((error) => console.error('MongoDB connection failed: ', error))
+
 app.listen(PORT, () => console.log('server listening on port', PORT))
