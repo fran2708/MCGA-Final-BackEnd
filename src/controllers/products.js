21ea98a75635d0e2e05a950c0510cd1cdfafb0a8
fran2708 <francopaladini2708@gmail.com>
2024-03-07 20:10:14 -0300

    feat: refactor of products routes, moved functionality to a controller

---
+++ src/controllers/products.js
@@ -0,0 +1,113 @@
+const productSchema = require('../models/Product')
+
+// GET PRODUCTS
+const getProducts = async (req,res) => {
+    try{
+        const response = await productSchema.find()
+        return res.status(200).json({
+            data: response,
+            error: false,
+            msg: 'List of products'
+        })
+    }catch(err){
+        return res.status(400).json({
+            error: true,
+            msg: err
+        })
+    }
+}
+
+// GET PRODUCT BY ID
+const getProductById = async (req,res) => {
+    try{
+        const response = await productSchema.findOne({ id: req.params.id })
+
+        if(!response){
+            return res.status(404).json({
+                error: true,
+                msg: 'Product not found'
+            })
+        }
+
+        return res.status(200).json({
+            data: response,
+            error: false,
+            msg: `Found product: ${response.name}`
+        })
+    }catch(err){
+        return res.status(400).json({
+            error: true,
+            msg: err
+        })
+    }
+}
+
+// ADD PRODUCT
+const addProduct = async (req,res) => {
+    try{
+        const product = new productSchema(req.body)
+        const newProduct = await product.save()
+        return res.status(201).json({
+            data: newProduct,
+            error: false,
+            msg: 'Product added'
+        })
+    }catch(err){
+        return res.status(400).json({
+            error: true,
+            msg: err
+        })
+    }
+}
+
+// UPDATE PRODUCT
+const updateProduct = async (req,res) => {
+    try{
+        const response = await productSchema.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+        if(!response){
+            return res.status(400).json({
+                error: true,
+                msg: 'Could not update product'
+            })
+        }
+
+        return res.status(200).json({
+            data: response,
+            error: false,
+            msg: 'Product updated'
+        })
+    }catch(err){
+        return res.status(400).json({
+            error: true,
+            msg: err
+        })
+    }
+}
+
+// DELETE PRODUCT
+const deleteProduct = async (req,res) => {
+    try{
+        const response = await productSchema.deleteOne({_id: req.params.id})
+
+        if(!response){
+            return res.status(404).json({
+                error: true,
+                msg: 'Could not delete product'
+            })
+        }
+
+        return res.status(204).json({
+            data: response,
+            error: false,
+            msg: 'Product deleted'
+        })
+    }catch(err){
+        return res.status(400).json({
+            error: true,
+            msg: err
+        })
+    }
+}
+
+module.exports = { getProducts, getProductById, addProduct, updateProduct, deleteProduct }
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,50 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const productSchema = require('../models/Product')
+const productsController = require('../controllers/products')
 
 // ADD PRODUCT
-router.post('/products', (req, res) => {
-    const product = productSchema(req.body)
-    product
-        .save(product)
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+router.post('/products', productsController.addProduct)
 
 // GET PRODUCTS
-router.get('/products', (req, res) => {
-    productSchema
-        .find()
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }))
-})
+router.get('/products', productsController.getProducts)
 
 // GET PRODUCT BY ID
-router.get("/products/:id", (req, res) => {
-    const { id } = req.params;
-    productSchema
-        .findById(id)
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-});
+router.get("/products/:id", productsController.getProductById);
 
 // UPDATE PRODUCT
-router.put("/products/:id", (req, res) => {
-    const { id } = req.params;
-    const { name, price, stock, description } = req.body;
-    productSchema
-        .updateOne({ _id: id }, { $set: { name, price, stock, description } })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-});
+router.put("/products/:id", productsController.updateProduct);
 
 // DELETE PRODUCT
-router.delete("/products/:id", (req, res) => {
-    const { id } = req.params;
-    productSchema
-        .deleteOne({ _id: id })
-        .then((data) => res.json(data))
-        .catch((error) => res.json({ message: error }));
-  });
+router.delete("/products/:id", productsController.deleteProduct);
 
 module.exports = router
