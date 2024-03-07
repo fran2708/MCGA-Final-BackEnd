691234f5ab470fd733834fb04bd005627c50e520
fran2708 <francopaladini2708@gmail.com>
2024-03-07 00:55:38 -0300

    feat: added rest of CRUD routes for products

--- src/routes/product.js
+++ src/routes/product.js
@@ -11,4 +11,40 @@ router.post('/products', (req, res) => {
         .catch((error) => res.json({ message: error }))
 })
 
+// GET PRODUCTS
+router.get('/products', (req, res) => {
+    productSchema
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+})
+
+// GET PRODUCT BY ID
+router.get("/products/:id", (req, res) => {
+    const { id } = req.params;
+    productSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+// UPDATE PRODUCT
+router.put("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, price, stock, description } = req.body;
+    productSchema
+        .updateOne({ _id: id }, { $set: { name, price, stock, description } })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+// DELETE PRODUCT
+router.put("/products/:id", (req, res) => {
+    const { id } = req.params;
+    productSchema
+      .remove({ _id: id })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
 module.exports = router
